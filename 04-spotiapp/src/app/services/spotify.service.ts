edcaca37979427832c4c4f0c4e688456b7a7786c
isidro <renplagnes@gmail.com>
2024-10-04 21:11:33 +0200

    Fix:APPSpoty

---
+++ 04-spotiapp/src/app/services/spotify.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+  constructor(private http:HttpClient) {
+    console.log('Spotify service listo');
+  }
+  getQuery(query: string){
+    const url =`https://api.spotify.com/v1/${query}`;
+    const headers = new HttpHeaders({
+      'Authorization':'Bearer QAeXhG6VN5GLb3vRB3Ft5yE2Tre0q8x4M851wz5FcAUqIWBbw6TpPXAIOjIx8d4GbqCk2sn3CSkwk96JQA'
+          });
+          return this.http.get(url,{headers});
+  }
+  getNewReleases(){
+  
+ return this.getQuery('browse/new-releases?limit=20')
+ .pipe(map(data=> data['albums'].items));
+
+
+
+  }
+  getArtistas(termino: string){
+    
+       return this.getQuery(`search?q=${termino}&type=artist&limit=15`)
+       .pipe(map(data=>data['artists'].items));
+      
+       
+        }
+        getArtista(id: string){
+    
+          return this.getQuery(`artists/${id}`)
+          //.pipe(map(data=>data['artists'].items));
+         
+        }   
+        getTopTracks(id: string){
+    
+          return this.getQuery(`artists/${id}/top-tracks?country=us`)
+          .pipe(map(data=>data['tracks']));
+         
+        }  
+      }
+    
